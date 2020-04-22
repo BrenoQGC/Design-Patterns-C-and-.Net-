e86f2702f068a5e48783b369d00855fad0431ffa
Breno Quintino <brenoqcarvalho@gmail.com>
2020-04-22 12:36:27 -0300

    First Commit

---
+++ 1.Single Responsibility Principle/DesignPatterns/SingleResponsabilityPrinciple.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static System.Console;
+
+namespace DesignPatterns
+{
+    public class SingleResponsabilityPrinciple
+    {
+        private readonly List<String> entries = new List<String>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; //memento pattern
+        }
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(SingleResponsabilityPrinciple j, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+                File.WriteAllText(filename, j.ToString());
+        }
+    }
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var j = new SingleResponsabilityPrinciple();
+            j.AddEntry("Its sunny today");
+            j.AddEntry("I drank some coffe");
+            WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"c:\temp\journal.txt";
+            p.SaveToFile(j, filename, true);
+            Process.Start(filename);
+        }
+    }
+}
---
+++ 2.OpenClosedPrinciple/OpenClosedPrinciple/Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace OpenClosedPrinciple
+{
+    public enum Color
+    {
+        Red, Green, Blue
+    }
+    public enum Size
+    {
+        Small, Medium, Large, Huge
+    }
+
+    public class Product
+    {
+        public string Name;
+        public Color Color;
+        public Size Size;
+        public Product(string name, Color color, Size size)
+        {
+            if (name == null)
+            {
+                throw new ArgumentNullException(paramName: nameof(name));
+            }
+            Name = name;
+            Color = color;
+            Size = size;
+        }
+    }
+
+    public class ProductFilter
+    {
+        //Filtro estatico:
+        public IEnumerable<Product> FilterBySize(IEnumerable<Product> products, Size size)
+        {
+            foreach (var p in products)
+                if (p.Size == size)
+                    yield return p;
+        }
+        public IEnumerable<Product> FilterByColor(IEnumerable<Product> products, Color color)
+        {
+            foreach (var p in products)
+                if (p.Color == color)
+                    yield return p;
+        }
+        public IEnumerable<Product> FilterBySizeAndColor(IEnumerable<Product> products, Size size, Color color)
+        {
+            foreach (var p in products)
+                if (p.Size== size && p.Color == color)
+                    yield return p;
+        }
+        //filtro aberto
+
+        public interface ISpecification<T>
+        {
+            bool IsSatisfied(T t);
+        }
+
+        public interface IFilter<T>
+        {
+            IEnumerable<T> Filter(IEnumerable<T> items, ISpecification<T> spec);
+        }
+        public class ColorSpecification : ISpecification<Product>
+        {
+            private Color color;
+
+            public ColorSpecification(Color color)
+            {
+                this.color = color;
+            }
+            public bool IsSatisfied(Product t)
+            {
+                return t.Color == color;
+            }
+        }
+
+        public class SizeSpecification : ISpecification<Product>
+        {
+            private Size size;
+
+            public SizeSpecification(Size size)
+            {
+                this.size = size;
+            }
+            public bool IsSatisfied(Product t)
+            {
+                return t.Size == size;
+            }
+        }
+
+        public class AndSpecification<T> : ISpecification<T>
+        {
+            private ISpecification<T> first, second;
+
+            public AndSpecification(ISpecification<T> first, ISpecification<T> second)
+            {
+                this.first = first ?? throw new ArgumentNullException(paramName: nameof(first));
+                this.second = second ?? throw  new ArgumentNullException(paramName:nameof(second));
+            }
+            public bool IsSatisfied(T t)
+            {
+                return first.IsSatisfied(t) && second.IsSatisfied(t);
+            }
+        }
+        public class BetterFilter : IFilter<Product>
+        {
+            public IEnumerable<Product> Filter(IEnumerable<Product> items, ISpecification<Product> spec)
+            {
+                foreach (var i in items)
+                    if (spec.IsSatisfied((i)))
+                        yield return i;
+            }
+        }
+    }
+    public class OpenClosedPrinciple
+    {
+        static void Main(string[] args)
+        {
+            var apple = new Product("Apple", Color.Green, Size.Small);
+            var tree = new Product("Tree", Color.Green, Size.Large);
+            var house = new Product("House", Color.Blue, Size.Large);
+
+            Product[] products = {apple, tree, house};
+            
+            var pf = new ProductFilter();
+            WriteLine("Green products (old):");
+            foreach (var p in pf.FilterByColor(products, Color.Green))
+            {
+                WriteLine($" - {p.Name} is green");
+            }
+            //implementação better filter
+
+            var bf = new ProductFilter.BetterFilter();
+            WriteLine("Green products");
+            foreach (var p in bf.Filter(products, new ProductFilter.ColorSpecification(Color.Green)))
+            {
+                WriteLine($" - {p.Name} is green");
+            }
+            WriteLine("Large Blue Items");
+            foreach (var p in bf.Filter(
+                products,
+                new ProductFilter.AndSpecification<Product>(new ProductFilter.ColorSpecification(Color.Blue),
+                    new ProductFilter.SizeSpecification(Size.Large))))
+            {
+                WriteLine($" - {p.Name} is large and blue");
+            }
+        }
+    }
+}
